3d9c6251af4b6bb54f107d7f127201c8f53f41e0
Ajay <ajayreddy.402@gmail.com>
2015-09-11 15:21:23 -0700

    my first commit

---
+++ src/simplelogin.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class simplelogin {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.facebook.com");
+		driver.findElement(By.xpath("//*[@id='email']")).sendKeys("priyanka.sankepally@gmail.com");
+		driver.findElement(By.xpath("//*[@id='pass']")).sendKeys("hfjhfjhgjfh");
+		
+
+	}
+
+}
